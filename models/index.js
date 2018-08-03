d797c6ad120d669af85163c80597e6401d36da4b
Raquel Queiroz <raquel.queiroz@youse.com.br>
2018-08-02 22:33:48 -0300

    create users has avengers (#3)

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 const Avenger = require('./avenger');
+const UsersHasAvenger = require('./user_has_avenger');
+// const User = require('./user');
+
+UserHasAvenger.hasMany(Avenger, { foreignKey: 'avengers_id'} )
+Avenger.belongsToMany(UserHasAvenger, { foreignKey: 'avengers_id' })
+UserHasAvenger.hasMany(User, { foreignKey: 'users_id'} )
+User.belongsToMany(UserHasAvenger, { foreignKey: 'users_id' })
 
 module.exports = exports = {
   Avenger
+  // UserHasAvenger
 };
